1725fd2578586f078e1af2339464b34598a12b20
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-02-08 20:20:24 +0000

    updated scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,18 @@ require 'pry'
 
 class FloweringTrees::Scraper
   
+  BASEURL = "https://www.willisorchards.com/category/flowering-trees"
+  
 
   def self.scrape
-    page = Nokogiri::HTML(open("https://www.willisorchards.com/category/flowering-trees"))
-    fl_trees = page.css("div.child-table").each do |trees|
+    page = Nokogiri::HTML(open("https://www.willisorchards.com/category/flowering-trees#.Xj7pAGhKg2w"))
+    list = page.css("div.child-link")
+    
+    fl_trees = list.map do |trees|
     url = trees.css("a").attr("href").value,
     name = trees.css("a").text,
     price = trees.css("p.strong").text
+    full_url = BASEURL + url
     
     FloweringTrees::Trees.new(url, name, price)
   end
