9c378141710e68a458133ba1446c29f9044f4959
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-02-16 04:59:23 +0000

    updated list variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,17 +9,15 @@ class FloweringTrees::Scraper
 
   def self.scrape_page 
     page = Nokogiri::HTML(open("https://www.willisorchards.com/category/flowering-trees#.Xj-qV2hKg2w"))
-    list = page.css("div.child-link")
-    trees = []
+    list = page.css("div.child-link a")
+    
     #binding.pry 
     list.map {|tree| tree.text} 
-      trees_hash = {
-    #url => trees.css("a").attr("href").value,
-    name => trees.css("a").text,
-    price => trees.css("p.strong").text
-      }
       
-      trees << tree_hash
+    #url => trees.css("a").attr("href").value,
+    name = trees.css("a").text,
+    price = trees.css("p.strong").text
+     
     FloweringTrees::Trees.new(name, price)
     end
     
